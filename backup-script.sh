d6809e63adc44e6cea0fe58f0cc4b1fc52ce4173
Vlad Hudaev <Vampir2090S@mail.ru>
2024-05-14 23:27:59 +0700

    Create backup-script.sh

---
+++ backup-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#simple backup script from sysahelper
+
+echo "Start backup!"
+
+backup_dir="/etc"
+dest_dir="/opt/backup"
+
+mkdir -p $dest_dir
+tar -czf $dest_dir/$(hostname -s)-$(date +"%d.%m.%y").tgz $backup_dir
+
+echo "Done!"
